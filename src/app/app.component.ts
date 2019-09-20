979fafae309998f230ec7da746f2275b7ed7d283
carluhsheyn <karlashanelaran@gmail.com>
2019-09-20 14:34:55 +0800

    Fixed Pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,12 @@ export class AppComponent {
   public inputList : any = [];
   public pageNumber : number = 0;
   public inputQuery = new FormControl('');
-  public inputResult: string = "";
+  public inputResult: string = null;
   public totalResults = null;
   public selectedPhoto = null;
   public isLoading : boolean = false;
   public inputRequired: string = "";
+  public totalPage = null;
 
   constructor(private _flickrService : FlickrService) { }
 
@@ -31,7 +32,7 @@ export class AppComponent {
 
   searchQuery() {
     if (this.inputQuery.value == "") {
-
+      this.inputRequired = "Input Required";
     } else {
       this.isLoading = true;
       this._flickrService.getPhoto(this.pageNumber, this.inputQuery.value).subscribe(result => {
@@ -40,6 +41,11 @@ export class AppComponent {
         this.totalResults = result.photos.total;
         this.isLoading = false;
         this.pageNumber = 1;
+        this.totalPage = result.photos.pages;
+        this.inputRequired = "";
+
+        console.log(this.pageNumber);
+        console.log(this.totalPage);
       })
     }
   }
